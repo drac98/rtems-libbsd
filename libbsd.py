4f8522d8d0302a28cdab56c75b3b57bc749ce5e3
Husni Faiz <ahamedhusni73@gmail.com>
2021-07-04 01:48:37 +0530

    libbsd.py:include usb serial port template

--- libbsd.py
+++ libbsd.py
@@ -1181,6 +1181,7 @@ class dev_usb_template(builder.Module):
             [
                 'sys/dev/usb/template/usb_template.c',
                 'sys/dev/usb/template/usb_template_cdce.c',
+                'sys/dev/usb/template/usb_template_modem.c',
             ],
             mm.generator['source']()
         )
