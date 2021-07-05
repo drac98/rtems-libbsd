94190fbdbc7d148492e423fdc89821befd7525c2
Husni Faiz <ahamedhusni73@gmail.com>
2021-07-06 00:44:44 +0530

    nexus-devices: Include CDC Ethernet for Beagle
    Configure bus for CDC Ethernet
    Include CDC Ethernet in Beagle BSP

--- rtemsbsd/include/machine/rtems-bsd-nexus-bus.h
+++ rtemsbsd/include/machine/rtems-bsd-nexus-bus.h
@@ -51,6 +51,8 @@
  *   RTEMS_BSD_DRIVER_MMC
  *   RTEMS_BSD_DRIVER_USB
  *   RTEMS_BSD_DRIVER_USB_MASS
+ *   RTEMS_BSD_DRIVER_USB_TEMPLATE
+ *   RTEMS_BSD_DRIVER_CDCE
  *
  *  Networking:
  *   RTEMS_BSD_DRIVER_SMC0
@@ -363,6 +365,15 @@ extern "C" {
 #endif /* RTEMS_BSD_DRIVER_USB_MASS */
 
 /*
+ * USB Template base driver.
+ */
+#if !defined(RTEMS_BSD_DRIVER_USB_TEMPLATE)
+  #define RTEMS_BSD_DRIVER_USB_TEMPLATE           \
+    SYSINIT_REFERENCE(usb_temp_init);             \
+    SYSINIT_DRIVER_REFERENCE(usb_template, usb)
+#endif /* RTEMS_BSD_DRIVER_USB_TEMPLATE */
+
+/*
  * USB SAF1761 host controller driver.
  */
 #if !defined(RTEMS_BSD_DRIVER_USB_SAF1761_OTG)
@@ -558,6 +569,14 @@ extern "C" {
   #define RTEMS_BSD_DRIVER_PCI_RE                 \
     SYSINIT_DRIVER_REFERENCE(re, pci);
 #endif /* RTEMS_BSD_DRIVER_PCI_RE */
+
+/*
+ * CDC Ethernet Driver.
+ */
+#if !defined(RTEMS_BSD_DRIVER_CDCE)
+  #define RTEMS_BSD_DRIVER_CDCE                   \
+    SYSINIT_DRIVER_REFERENCE(cdce, uhub);
+#endif /* RTEMS_BSD_DRIVER_CDCE */
 
 /**
  ** MMI Physical Layer Support.
